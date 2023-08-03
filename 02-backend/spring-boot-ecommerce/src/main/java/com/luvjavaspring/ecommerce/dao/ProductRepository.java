9fdf78d6bc0367d04c6f65f7336c591b011c4785
jinjin1919 <75953443+jinjin1919@users.noreply.github.com>
2023-08-03 14:06:54 -0400

    first commit

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luvjavaspring/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.luvjavaspring.ecommerce.dao;
+
+import com.luvjavaspring.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
